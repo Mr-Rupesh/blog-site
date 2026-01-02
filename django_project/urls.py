4e3635438a9f71f30f09159c1e053aa04f5e58bf
Rupesh M <rupeshmalhipparge@gmail.com>
2026-01-02 15:36:44 +0530

    user accounts, and static files

--- django_project/urls.py
+++ django_project/urls.py
@@ -20,5 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("blog.urls"))
+    path("account/", include("django.contrib.auth.urls")),
+    path("accounts/", include("accounts.urls")),
+    path("", include("blog.urls")),
 ]
